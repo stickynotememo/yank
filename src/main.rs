097f900e8c3c7a19f17416196e8e56f3d2ca577a
stickynotememo <a@a.com>
2025-07-16 11:36:12 +1000

    Created paste functionality

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,12 @@ struct Args {
     paste_file: Option<String>,
 }
 
+#[derive(PartialEq, Clone, Copy)]
+enum MoveOp {
+    Copy,
+    Move
+}
+
 fn copy(args: &Args) {
     let file = PathBuf::from(args.file.as_ref().unwrap());
     // TODO: Add support for non UTF-8 characters by avoiding unwrap and using let else to
@@ -45,27 +51,39 @@ fn copy(args: &Args) {
 
 }
 
-fn cut() {
-
-}
-
 fn paste(args: &Args) {
     // No file specified, yank should paste the file in the clipboard
     // Optionally, the --paste flag can be used to specify where to save the file
 
-    let clipboard = env::var("YANK_FILE_CLIPBOARD").expect("yank: no file in clipboard"); 
+    // If either of these variables aren't set, yank hasn't been run yet in copy or cut mode.
+    // TODO: Use a more readable method to convert to a static string (e.g. to_str())
+    let moveop: MoveOp = match &env::var("YANK_FILE_MOVEOP").expect("yank: no file in clipboard")[..] {
+        "MOVE" => MoveOp::Move,
+        "COPY" => MoveOp::Copy,
+        _ => {
+            // If YANK_FILE_MOVEOP is set, no other value than the above two should be possible.
+            unreachable!()
+        }
+    };
+    let clipboard_string = &env::var("YANK_FILE_CLIPBOARD").expect("yank: no file in clipboard"); 
+    let clipboard = Path::new(clipboard_string);
 
     let paste_file_name: &Path = match &args.paste_file {
         // If a paste file has been specified using the flag, it should be used instead
         // of the file used in the clipboard
+        // If the clipboard value is being used, the file/directory should be pasted in the
+        // current directory while maintaining its filename
         Some(paste_file) => Path::new(paste_file),
-        None => { Path::new(&clipboard) }
+        None => { Path::new(clipboard.file_name().unwrap()) } 
+        // Clipboard is set by yank, not by the user. Using unwrap is okay.
     };
 
     let paste_file_path = path::absolute(paste_file_name).expect("yank: paste path could not be parsed");
-    
-    dbg!(paste_file_path);
 
+    match moveop {
+        MoveOp::Move => { rename(clipboard, paste_file_path).expect("yank: filesystem error") },
+        MoveOp::Copy => { todo!() }
+    }
 
 }
 
@@ -73,11 +91,7 @@ fn main() {
     let args = Args::parse();
     match &args.file {
         Some(file) => {
-            if args.cut {
-                cut();
-            } else {
-                copy(&args);
-            }
+            copy(&args);
         },
         None => paste(&args)
     };
